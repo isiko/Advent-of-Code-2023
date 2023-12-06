d8ca34832f705455779d3895435b912e3c3ec502
isiko <mail@isiko404.dev>
2023-12-06 23:57:25 +0100

    Update Build script

--- runAll.sh
+++ runAll.sh
@@ -1,2 +1,2 @@
-find -maxdepth 1 -type d -name "day*" | map d 'cd "$d" && cargo build -q'
-find ./target/debug -maxdepth 1 -type f -iregex '^.*/day[0-9]+_[0-9]$' | sort | map e 'time "$e"'
+cargo build --release --bins
+find ./target/release -maxdepth 1 -type f -iregex '^.*/day[0-9]+_[0-9]$' | sort | map e 'time "$e"'
