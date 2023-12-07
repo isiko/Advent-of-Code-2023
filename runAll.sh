334dfeea2f3ad0dfb8ac0db390df5457cad56a8c
isiko <mail@isiko404.dev>
2023-12-07 17:17:26 +0100

    Update ./runAll.sh

--- runAll.sh
+++ runAll.sh
@@ -1,2 +1,2 @@
 cargo build --release --bins
-find ./target/release -maxdepth 1 -type f -iregex '^.*/day[0-9]+_[0-9]$' | sort | map e 'time "$e"&&echo ""'
+find ./target/release -maxdepth 1 -type f -iregex '^.*/day[0-9]+_[0-9]$' | sort | xargs hyperfine -N
