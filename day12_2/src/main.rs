8bbe1cb666ef735dede44cda6bfe02b22144bafa
isiko <mail@isiko404.dev>
2023-12-12 14:55:42 +0100

    More Preformance Improvements

--- day12_2/src/main.rs
+++ day12_2/src/main.rs
@@ -1,99 +1,113 @@
+use rstest::rstest;
 use std::{sync::mpsc, thread};
 
 fn main() {
     const INPUT: &str = include_str!("input");
     let (tx, rx) = mpsc::channel();
 
+    let mut started_threads = 0;
+
     for line in INPUT.lines() {
         if line.starts_with("//") {
             continue;
         }
+        started_threads += 1;
         let line_tx = tx.clone();
+        let mut split = line.split(" ");
+        let data = split.next().unwrap();
+        let check = split
+            .next()
+            .unwrap()
+            .split(",")
+            .map(|g| g.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+
+        let mut data_actual = data.to_string();
+        for _ in 0..4 {
+            data_actual.push('?');
+            data_actual.push_str(data);
+        }
+
+        let mut check_actual = check.clone();
+        check_actual.extend(check.clone());
+        check_actual.extend(check.clone());
+        check_actual.extend(check.clone());
+        check_actual.extend(check.clone());
         thread::spawn(move || {
-            let mut split = line.split(" ");
-            let data = split.next().unwrap();
-            let check = split
-                .next()
-                .unwrap()
-                .split(",")
-                .map(|g| g.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>();
-
-            let mut data_actual = data.to_string();
-            for _ in 0..4 {
-                data_actual.push('?');
-                data_actual.push_str(data);
-            }
-
-            let mut check_actual = check.clone();
-            check_actual.extend(check.clone());
-            check_actual.extend(check.clone());
-            check_actual.extend(check.clone());
-            check_actual.extend(check.clone());
-
             let version = parse_line(
                 '.',
                 data_actual.to_string(),
                 check_actual.clone(),
-                "".to_string(),
+                &"".to_string(),
             );
-            println!("{} => {}", line, version);
+            //println!("{} => {}", line, version);
             line_tx.send((line.to_string(), version)).unwrap();
         });
     }
+    let mut ended_threads = 0;
     let mut result = 0;
     for recieved in rx {
-        let (line, output): (String, u32) = recieved;
+        let (line, output): (String, usize) = recieved;
         println!("{} => {}", line, output);
         result += output;
-        println!("Result: {}", result);
+        ended_threads += 1;
+        println!(
+            "Result {} of {}: {}",
+            ended_threads, started_threads, result
+        );
     }
 
     println!("Day 12 Task 1: {}", result);
 }
 
-fn parse_line(last_char: char, line: String, mut check: Vec<u32>, mut word: String) -> u32 {
-    if line.len() == 0 {
+fn parse_line(last_char: char, old_line: String, mut check: Vec<usize>, word: &String) -> usize {
+    if old_line.len() == 0 {
         //println!("{}", word);
         //println!("<============== Found =============>");
         return 1;
     } else {
-        let check_sum = check.iter().sum::<u32>();
-        let h_sum = line.chars().filter(|c| *c == '#').count() as u32;
-        let q_sum = line.chars().filter(|c| *c == '?').count() as u32;
-        let char = line.chars().nth(0).unwrap();
-        let line: String = line.chars().skip(1).collect();
-
-        //println!("{} {} {} : {:?}", word, char, line, check);
+        let mut q_sum = 0;
+        let mut h_sum = 0;
+        let mut line = "".to_string();
+        let mut char = ' ';
+        for (i, c) in old_line.chars().enumerate() {
+            match c {
+                '?' => {
+                    q_sum += 1;
+                }
+                '#' => {
+                    h_sum += 1;
+                }
+                _ => {}
+            }
+            if i == 0 {
+                char = c;
+            } else {
+                line.push(c);
+            }
+        }
 
         if check.len() == 0 {
             if h_sum > 0 {
-                //println!("No ranges but still Chars");
                 return 0;
             }
         } else {
             if check[0] == 0 {
                 if last_char == '#' && char == '.' {
-                    check = check.into_iter().skip(1).collect();
+                    check.remove(0);
                 } else if last_char == '#' && char == '#' {
-                    //println!("New Range not Allowed");
                     return 0;
                 }
             } else {
                 if last_char == '#' && char == '.' {
-                    //println!("Range not done");
                     return 0;
                 }
             }
         }
 
+        let check_sum = check.iter().sum::<usize>();
+
         if !(h_sum..(h_sum + q_sum + 1)).contains(&check_sum) {
-            //println!(
-            //    "Not enough Chars to Match ({}..{} <=> {})",
-            //    h_sum,
-            //    h_sum + q_sum + 1,
-            //    check_sum
-            //);
             return 0;
         }
 
@@ -103,73 +117,41 @@ fn parse_line(last_char: char, line: String, mut check: Vec<u32>, mut word: Stri
                 check[0] -= 1;
             }
             '?' => {
-                let mut h_string = "".to_string();
-                h_string.push('#');
-                h_string.push_str(&line);
+                let mut h_string = old_line.clone();
+                h_string.replace_range(0..1, "#");
+                let mut d_string = old_line;
+                d_string.replace_range(0..1, ".");
 
-                let mut d_string = "".to_string();
-                d_string.push('.');
-                d_string.push_str(&line);
-
-                return parse_line(last_char, h_string, check.clone(), word.clone())
-                    + parse_line(last_char, d_string, check.clone(), word.clone());
+                return parse_line(last_char, h_string, check.clone(), word)
+                    + parse_line(last_char, d_string, check, word);
             }
             _ => {
                 panic!("Unknown Character")
             }
         }
+        let mut word = word.clone();
         word.push(char);
-        return parse_line(char, line, check, word);
+        return parse_line(char, line, check, &word);
     }
 }
-//fn parse_line(line: String, mut check: Vec<u32>, last_char: char) -> u32 {
-//    if line.len() == 0 {
-//        return 1;
-//    } else {
-//        let check_sum = check.iter().sum::<u32>();
-//        let h_sum = line.chars().filter(|c| *c == '#').count() as u32;
-//        let q_sum = line.chars().filter(|c| *c == '?').count() as u32;
-//
-//        if !(h_sum..(h_sum + q_sum + 1)).contains(&check_sum) {
-//            return 0;
-//        }
-//
-//        if check.len() == 0 && h_sum + q_sum > 0 {
-//            return 0;
-//        }
-//
-//        let char = line.chars().nth(0).unwrap();
-//        let line: String = line.chars().skip(1).collect();
-//
-//        if check[0] == 0 && char == '#' {
-//            return 0;
-//        }
-//
-//        match char {
-//            '.' => {
-//                if check[0] == 0 {
-//                    check = check.into_iter().skip(1).collect();
-//                }
-//            }
-//            '#' => {
-//                check[0] -= 1;
-//            }
-//            '?' => {
-//                let sum = parse_line(line.clone(), check.clone(), char);
-//                if check[0] == 0 {
-//                    return sum;
-//                }
-//                check[0] -= 1;
-//                if check[0] == 0 {
-//                    check = check.into_iter().skip(1).collect();
-//                }
-//                return sum + parse_line(line, check, char);
-//            }
-//            _ => {
-//                panic!("Unknown Character")
-//            }
-//        }
-//
-//        parse_line(line, check, char)
-//    }
-//}
+
+#[rstest]
+#[case("???.### 1,1,3", 1)]
+#[case("????.#...#... 4,1,1", 1)]
+#[case(".??..??...?##. 1,1,3", 4)]
+#[case("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
+#[case("????.######..#####. 1,6,5", 4)]
+#[case("?###???????? 3,2,1", 10)]
+fn test_line(#[case] line: String, #[case] expected: usize) {
+    let mut split = line.split(" ");
+    let data = split.next().unwrap();
+    let check = split
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|g| g.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    let result = parse_line('.', data.to_string(), check.clone(), &"".to_string());
+    assert_eq!(result, expected);
+}
